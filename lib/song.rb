46c0382ddafb50b3a6e6b938c26ac39eef2f0ab0
Cameron Cousins <cameroncousins18@gmail.com>
2020-09-24 22:19:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,4 +29,23 @@ class Song
     song
   end 
   
+  def artist
+    @artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre
+    @genre
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  
 end
